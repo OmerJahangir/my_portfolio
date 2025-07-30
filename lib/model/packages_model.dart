6dc906977dbe26377f4ecb5370fab689cc075a96
Omer Jahangir <omerjahangir31@gmail.com>
2025-07-30 15:03:18 +0500

    links added

--- lib/model/packages_model.dart
+++ lib/model/packages_model.dart
@@ -12,4 +12,9 @@ List<PackageModel> packageList = [
     'A beautiful, fully customizable Flutter widget that mimics an incoming call slider — swipe right to accept, swipe left to decline.',
     'https://pub.dev/packages/call_slider_button',
   ),
+  PackageModel(
+    'Bouncing Call Slider',
+    'A highly customizable Flutter widget that mimics an incoming call slider — swipe up to answer or down to decline with bouncing, animated, and haptic feedback effects.',
+    'https://pub.dev/packages/bouncing_call_slider',
+  ),
 ];
